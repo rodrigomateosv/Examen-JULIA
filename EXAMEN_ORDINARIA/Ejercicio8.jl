297423a0ae05a6acd687419ec707a8c6664b9bb2
rodrigomateosv <114070557+rodrigomateosv@users.noreply.github.com>
2023-06-01 12:33:25 +0200

    344

---
+++ EXAMEN_ORDINARIA/Ejercicio8.jl
@@ -0,0 +1,5 @@
+Calcular la coordenada x del centroide de la región encerrada entre las gráficas de las funciones f(x) = sqrt(x-2) y g(x) = x^2-6x+8
+
+# funciones
+f(x) = sqrt(x-2)
+g(x) = x^2-6x+8
