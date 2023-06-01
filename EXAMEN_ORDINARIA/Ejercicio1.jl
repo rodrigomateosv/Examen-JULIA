be2a40d0e5ee6de4d64406982820f4edad55b1ad
rodrigomateosv <114070557+rodrigomateosv@users.noreply.github.com>
2023-06-01 11:58:45 +0200

    gg

---
+++ EXAMEN_ORDINARIA/Ejercicio1.jl
@@ -0,0 +1,4 @@
+Calcular la suma parcial sum from i equals 0 to 25 of fraction numerator 2 to the power of n over denominator left parenthesis n plus 1 right parenthesis factorial end fraction con 20 decimales.
+
+#sumatorio
+An= 
---
+++ EXAMEN_ORDINARIA/Ejercicio2.jl
@@ -0,0 +1,2 @@
+Calcular la suma parcial de orden 100 de la serie de Maclaurin de la función $f(x)=\ln(1+x)$ en x equals 1 divided by 2 con 10 decimales.
+
