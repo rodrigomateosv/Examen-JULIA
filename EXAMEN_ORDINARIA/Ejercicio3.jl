8603452222aa4fb204cec2a764df13e8388f45e6
rodrigomateosv <114070557+rodrigomateosv@users.noreply.github.com>
2023-06-01 12:02:10 +0200

    44

---
+++ EXAMEN_ORDINARIA/Ejercicio3.jl
@@ -0,0 +1,4 @@
+Calcular el volumen del sólido de revolución que se obtiene al rotar alrededor del eje x la gráfica de la función f f(x)=exp(-x/2)  en el intervalo [0,2].
+
+#funcion
+f(x)=exp(-x/2)
