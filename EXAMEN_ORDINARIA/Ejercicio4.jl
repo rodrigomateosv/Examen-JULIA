b7c71c8a5422de6e58958c86d64ba5f4ecee5455
rodrigomateosv <114070557+rodrigomateosv@users.noreply.github.com>
2023-06-01 12:07:16 +0200

    88

---
+++ EXAMEN_ORDINARIA/Ejercicio4.jl
@@ -0,0 +1,15 @@
+¿Cuál de las siguientes funciones es una función de densidad de probabilidad?
+
+
+   
+#funciones
+
+A) Ninguna de ellas
+
+b)f(x)=3*e^(-2*x)/(1+e^(-x))^3
+
+c)g(x)=1/2*e^(-abs(x)/2)
+
+d)h(x)=2/(pi*(x^2+4))
+
+f)j(x)=x/(1+x^2)^2 x>=0
