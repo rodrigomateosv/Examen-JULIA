cd76a35a51b88930f7d1161b85789d3686ba2971
rodrigomateosv <114070557+rodrigomateosv@users.noreply.github.com>
2023-06-01 12:15:29 +0200

    99

---
+++ EXAMEN_ORDINARIA/Ejercicio9.jl
@@ -0,0 +1,7 @@
+Calcular la suma inferior de Riemann de la función f f(x) = 1/sqrt(x + 1) en el intervalo left [2,4], tomando una partición de 50 subinvervalos de igual amplitud.
+
+#funcion
+f(x) = 1/sqrt(x + 1)
+
+#intervalo
+a, b = 2, 4
